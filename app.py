7225e283dddefe013f9f5dcd6be9633582315bb6
dpescado <dja.pescado@gmail.com>
2019-01-19 17:19:44 +0800

    update python file

--- app.py
+++ app.py
@@ -1,34 +1,32 @@
-import io
-import os
-import re
 import base64
-from datetime import datetime as dt
+import os
 from urllib.parse import quote as urlquote
 
 from flask import Flask, send_from_directory
-
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output, State
+from dash.dependencies import Input, Output
 
 import dash_bootstrap_components as dbc
 
 import pandas as pd
 
-UPLOAD_DIRECTORY = '/project_marketo_app/app_uploaded_files'
+UPLOAD_DIRECTORY = "/Project_Marketo_App/app_uploaded_files"
 
 if not os.path.exists(UPLOAD_DIRECTORY):
     os.makedirs(UPLOAD_DIRECTORY)
 
-# create a route for downloading files directly
+# Normally, Dash creates its own Flask server internally. By creating our own,
+# we can create a route for downloading files directly:
 server = Flask(__name__)
 app = dash.Dash(server=server)
 
 
-@server.route('download/<path:path>')
+@server.route("/download/<path:path>")
 def download(path):
-    return send_from_directory(UPLOAD_DIRECTORY, path, as_attachement=True)
+    """Serve a file from the upload directory."""
+    return send_from_directory(UPLOAD_DIRECTORY, path, as_attachment=True)
 
 
 # app header
@@ -51,7 +49,8 @@ tab1_content = (
                             [
                                 dcc.Upload(
                                     dbc.Button('Upload DY-SOI Data'),
-                                    id='id-upload1')
+                                    id='id-upload1',
+                                    multiple=True)
                             ], width=3
                         ),
                     ]
@@ -183,12 +182,14 @@ app.layout = html.Div([_navbar, _body])
 
 
 def save_file(name, content):
-    data = content.encode('utf8').split(b';base64,')[1]
-    with open(os.path.join(UPLOAD_DIRECTORY, name), 'wb') as fp:
+    """Decode and store a file uploaded with Plotly Dash."""
+    data = content.encode("utf8").split(b";base64,")[1]
+    with open(os.path.join(UPLOAD_DIRECTORY, name), "wb") as fp:
         fp.write(base64.decodebytes(data))
 
 
 def uploaded_files():
+    """List the files in the upload directory."""
     files = []
     for filename in os.listdir(UPLOAD_DIRECTORY):
         path = os.path.join(UPLOAD_DIRECTORY, filename)
@@ -197,28 +198,40 @@ def uploaded_files():
     return files
 
 
+def del_uploaded_file():
+    for filename in os.listdir(UPLOAD_DIRECTORY):
+        os.remove(os.path.join(UPLOAD_DIRECTORY, filename))
+
+
 def file_download_link(filename):
-    location = '/download/{}'.format(urlquote(filename))
+    """Create a Plotly Dash 'A' element that downloads a file from the app."""
+    location = "/download/{}".format(urlquote(filename))
     return html.A(filename, href=location)
 
 
 @app.callback(
     Output(component_id='id-lbl1', component_property='children'),
-    [Input(component_id='id-upload1', component_property='filename'),
-     Input(component_id='id-upload1', component_property='contents')]
+    [
+        Input(component_id='id-upload1', component_property='filename'),
+        Input(component_id='id-upload1', component_property='contents')
+    ]
 )
 def update_output(uploaded_filenames, uploaded_file_contents):
+    """Save uploaded files and regenerate the file list."""
+    del_uploaded_file()
     if uploaded_filenames is not None and uploaded_file_contents is not None:
         for name, data in zip(uploaded_filenames, uploaded_file_contents):
             save_file(name, data)
 
     files = uploaded_files()
     if len(files) == 0:
-        return [html.Li('No Files Yet')]
+        return [html.Li("No files yet!")]
+    elif len(files) == 1:
+        # return [html.Li(filename)for filename in files]
+        return [html.Li(file_download_link(files[0]))]
     else:
-        return [html.Li(file_download_link(filename)) for filename in files]
+        return [html.Li('Wrong Number of Files .. Please Check')]
 
 
-# app main
 if __name__ == "__main__":
     app.run_server(debug=True, port=8888)
